e6ba99e3a6dd5ca2fed425bd8ffb9edc845c5d9b
Corgo <94418843+TeamCorgo@users.noreply.github.com>
2025-10-28 09:28:19 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,3 +7,22 @@ if [ "$(id -u)" -ne 0 ]; then
     whiptail --title "Permission Denied" --msgbox "You must run this script as root!" 8 40
     exit 1
 fi
+
+# Disable ipv6, force ipv4
+echo "Acquire::ForceIPv4 \"true\";" >> /etc/apt/apt.conf.d/99force-ipv4
+
+# Ask for the primary username(to control their .bashrc)
+username=$(whiptail \
+    --backtitle "Debian Setup Wizard" \
+    --title "User Configuration" \
+    --inputbox "Enter your username:" 8 40 \
+    3>&1 1>&2 2>&3)
+echo "You entered: $username"
+
+# Check if the .bashrc exists for that user
+if [ -f "/home/$username/.bashrc" ]; then
+    echo ".bashrc exists for user $username"
+else
+    echo ".bashrc does NOT exist for user $username"
+    exit 1
+fi
